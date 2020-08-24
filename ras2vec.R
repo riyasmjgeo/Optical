96ead636aff3df3be49f403de647ae95d21b3bb5
Riyas MJ <riyasmjgeo@gmail.com>
2020-08-24 13:18:02 +0530

    Update ras2vec.R

--- ras2vec.R
+++ ras2vec.R
@@ -1,15 +1,24 @@
 # Convert raster to vector
 # Assign breakpoints
 # Apply mask
+# Avoid pixels having NA values in other raster
 library(raster)
 library(rgdal)
-
-ras = raster("C:/Mega/Work_files/Results/SBAS/K-means/Vert_corr19_20.tif")
+b1 = raster("C:/Mega/Work_files/Results/SBAS/K-means/Vert_corr17_18.tif")
+b2 = raster("C:/Mega/Work_files/Results/SBAS/K-means/Vert_corr18_19.tif")
+b3 = raster("C:/Mega/Work_files/Results/SBAS/K-means/Vert_corr19_20.tif")
 shp = shapefile("C:/Mega/Work_files/Results/SBAS/Decompose/Verti_30mm.shp")
 
 #masking
-ras = mask(ras,shp)
+b1 [is.na(b2)] <- NA
+b1 [is.na(b3)] <- NA
+b2 [is.na(b1)] <- NA
+b2 [is.na(b3)] <- NA
+b3 [is.na(b1)] <- NA
+b3 [is.na(b2)] <- NA
 
+ras = b3             ## Edit here ###
+ras = mask(ras,shp)
 # assigning Break points
 ras = replace(ras,ras< -50, 201)
 ras = replace(ras,ras< -35, 202)
